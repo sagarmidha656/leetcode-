4c7693113a1b5a2e9f88d97c4593f481288c1599
sagarmidha656 <sagarmidha656@gmail.com>
2026-03-25 15:10:46 +0530

    Time: 1 ms (99.93%), Space: 42.8 MB (38.4%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -7,7 +7,8 @@ class Solution {
             int ld = x % 10;
               x /= 10 ;
          
-
+        if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && ld > 7)) return 0;
+         if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && ld < -8)) return 0;
 
               rev = (rev*10) + ld;
         }
