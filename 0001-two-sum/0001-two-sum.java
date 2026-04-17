4a0c532ee83d0fc661edf556c95f172c63d2fbab
sagarmidha656 <sagarmidha656@gmail.com>
2026-04-17 23:00:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,10 +4,10 @@ class Solution {
         for(int i = 0 ; i <nums.length;i++){
             for(int j = i+1 ; j<nums.length;j++){
                 if((nums[i]+nums[j])==target){
-                    // if(i>j){
-                    //     two[0]=j;
-                    //     two[1]=i;
-                    // }
+                    if(i>j){
+                        two[0]=j;
+                        two[1]=i;
+                    }
                     two[0]=i;
                     two[1]=j;
                 }
