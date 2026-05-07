ca242e64946eed26f82c7d37da55d2b343c84a3f
sagarmidha656 <sagarmidha656@gmail.com>
2026-05-08 00:21:11 +0530

    Time: 0 ms (100%), Space: 48.5 MB (31.5%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int s=0; 
+        int e = arr.length-1;
+        while(s<=e){
+            int mid = s+((e-s)/2);
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                e = mid - 1;
+            }
+            else{
+                s = mid +1;
+            }
+        }
+        return -1;
+    }
+}
