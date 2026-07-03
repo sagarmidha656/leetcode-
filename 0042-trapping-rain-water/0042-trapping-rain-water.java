3483fb682b8cce5263e1a1dbba755c9c06fa24d1
sagarmidha656 <sagarmidha656@gmail.com>
2026-07-04 03:21:55 +0530

    Time: 0 ms (100%), Space: 47.9 MB (45.65%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,52 @@
 class Solution {
     public int trap(int[] height) {
-       int n = height.length;
-       int count=0;
-       int[] left = new int[n];
-       int[] right = new int[n];
-       left[0] = height[0];
-       right[n-1]=height[n-1];
-      int i = 1 , j = n -2;
-      while(i<n && j >=0){
-        left[i] = Math.max(height[i],left[i-1]);
-        right[j] = Math.max(height[j],right[j+1]);
-        j--;i++;
-      }
-      for(int k =0 ;k<n;k++){
-       int x = Math.min(left[k],right[k]);
-       count += x-height[k];
-      }
-      return count;
+        int lmax = 0,rmax =0;
+        int count =0;
+        int n = height.length;
+        int i =0 , j = n-1;
+        while(i<=j){
+            if(height[i]<=height[j]){
+                lmax = Math.max(lmax,height[i]);
+                count += lmax - height[i];
+                i++;
+            }
+            else{
+                rmax = Math.max(rmax,height[j]);
+                count += rmax - height[j];
+                j--;
+            }
+        }
+        return count;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // -----Brute Force---------
+    //    int n = height.length;
+    //    int count=0;
+    //    int[] left = new int[n];
+    //    int[] right = new int[n];
+    //    left[0] = height[0];
+    //    right[n-1]=height[n-1];
+    //   int i = 1 , j = n -2;
+    //   while(i<n && j >=0){
+    //     left[i] = Math.max(height[i],left[i-1]);
+    //     right[j] = Math.max(height[j],right[j+1]);
+    //     j--;i++;
+    //   }
+    //   for(int k =0 ;k<n;k++){
+    //    int x = Math.min(left[k],right[k]);
+    //    count += x-height;
+    //   }
+    //   return count;
     }
 }
