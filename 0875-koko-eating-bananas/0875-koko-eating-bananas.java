f4d8c6343a8271f2e46476fc78a5a1f4b7a3465d
sagarmidha656 <sagarmidha656@gmail.com>
2026-07-04 04:21:22 +0530

    Time: 20 ms (35.41%), Space: 47.8 MB (58.48%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,48 @@
+class Solution {
+    public long totalhours(int[] piles , int mid){
+        long  hours = 0;
+        for(int i =0 ; i<piles.length;i++){
+            hours += (long)Math.ceil((double)piles[i]/mid);
+        }
+        return hours;
+    }
+    public int minEatingSpeed(int[] piles, int H) {
+        int maxx = piles[0];
+        for(int num : piles){
+            maxx= Math.max(maxx,num);
+        }
+        int l =1 , h= maxx;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            long hours = totalhours(piles,mid);
+            if(hours<=H){
+                h = mid -1;
+            }
+            else{
+                l = mid +1;
+            }
+        }
+        return l;
+
+
+
+
+
+
+        //-----------BRUTE FORCE ------------------------
+        // int maxx = piles[0];
+        // for(int num : piles){
+        //     maxx = Math.max(maxx,num);
+        // }
+        // for(int time = 1 ; time <= maxx ; time++){
+        //     int hours =0;
+        //     for(int i =0; i<piles.length;i++){
+        //     hours += (int)Math.ceil((double)piles[i]/time);
+        //     }
+        //     if(hours<=h){
+        //         return time;
+        //     }
+        // }
+        // return -1;
+    }
+}
